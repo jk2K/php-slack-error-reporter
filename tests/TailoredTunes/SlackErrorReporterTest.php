a66b7c0ccaa626560fa103f35ac4bc322157c7a4
meza <meza@meza.hu>
2014-09-12 10:52:28 +0100

    Fixed expectations

--- tests/TailoredTunes/SlackErrorReporterTest.php
+++ tests/TailoredTunes/SlackErrorReporterTest.php
@@ -67,29 +67,29 @@ class SlackErrorReporterTest extends \PHPUnit_Framework_TestCase
         $text = sprintf(
             "EXCEPTION - [0] %s:%d - %s\nStacktrace:\n\n%s",
             realpath(__FILE__),
-            __LINE__ + 17,
+            __LINE__ + 27,
             $msg,
             "#0 [internal function]: TailoredTunes\\SlackErrorReporterTest->testExceptionHandler()\n" .
-            "#1 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/Framework/TestCase.php(951):
-            ReflectionMethod->invokeArgs(Object(TailoredTunes\\SlackErrorReporterTest), Array)\n" .
-            "#2 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/Framework/TestCase.php(817):
-            PHPUnit_Framework_TestCase->runTest()\n" .
-            "#3 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/Framework/TestResult.php(686):
-            PHPUnit_Framework_TestCase->runBare()\n" .
-            "#4 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/Framework/TestCase.php(753):
-            PHPUnit_Framework_TestResult->run(Object(TailoredTunes\\SlackErrorReporterTest))\n" .
-            "#5 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/Framework/TestSuite.php(675):
-            PHPUnit_Framework_TestCase->run(Object(PHPUnit_Framework_TestResult))\n" .
-            "#6 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/Framework/TestSuite.php(675):
-            PHPUnit_Framework_TestSuite->run(Object(PHPUnit_Framework_TestResult))\n" .
-            "#7 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/TextUI/TestRunner.php(426):
-            PHPUnit_Framework_TestSuite->run(Object(PHPUnit_Framework_TestResult))\n" .
-            "#8 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/TextUI/Command.php(179):
-            PHPUnit_TextUI_TestRunner->doRun(Object(PHPUnit_Framework_TestSuite), Array)\n" .
-            "#9 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/TextUI/Command.php(132):
-            PHPUnit_TextUI_Command->run(Array, true)\n" .
-            "#10 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/phpunit(55):
-            PHPUnit_TextUI_Command::main()\n" .
+            "#1 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/Framework/TestCase.php(951): " .
+            "ReflectionMethod->invokeArgs(Object(TailoredTunes\\SlackErrorReporterTest), Array)\n" .
+            "#2 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/Framework/TestCase.php(817): " .
+            "PHPUnit_Framework_TestCase->runTest()\n" .
+            "#3 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/Framework/TestResult.php(686): " .
+            "PHPUnit_Framework_TestCase->runBare()\n" .
+            "#4 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/Framework/TestCase.php(753): " .
+            "PHPUnit_Framework_TestResult->run(Object(TailoredTunes\\SlackErrorReporterTest))\n" .
+            "#5 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/Framework/TestSuite.php(675): " .
+            "PHPUnit_Framework_TestCase->run(Object(PHPUnit_Framework_TestResult))\n" .
+            "#6 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/Framework/TestSuite.php(675): " .
+            "PHPUnit_Framework_TestSuite->run(Object(PHPUnit_Framework_TestResult))\n" .
+            "#7 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/TextUI/TestRunner.php(426): " .
+            "PHPUnit_Framework_TestSuite->run(Object(PHPUnit_Framework_TestResult))\n" .
+            "#8 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/TextUI/Command.php(179): " .
+            "PHPUnit_TextUI_TestRunner->doRun(Object(PHPUnit_Framework_TestSuite), Array)\n" .
+            "#9 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/src/TextUI/Command.php(132): " .
+            "PHPUnit_TextUI_Command->run(Array, true)\n" .
+            "#10 ".realpath(__DIR__.'/../../')."/vendor/phpunit/phpunit/phpunit(55): " .
+            "PHPUnit_TextUI_Command::main()\n" .
             "#11 {main}"
         );
         $this->slack->expects($this->once())->method('send')
